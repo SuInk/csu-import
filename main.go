07191255e2cbe47be8d3875b0ffabd01e4f85ab0
SuInk <xilei0804@qq.com>
2022-09-03 21:26:45 +0800

    增加前端展示

--- main.go
+++ main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"csu-import/controllers"
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.Static("/", "public")
 	// post 账号密码下载ics
 	e.POST("/api/download", controllers.Login)
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Fatal(e.Start(":9093"))
 }
